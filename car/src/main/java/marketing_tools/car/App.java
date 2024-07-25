47aafe68ff1f5f167d1442347060bc7d986e6e83
Kiranyenni <kiranyenni@users.noreply.github.com>
2024-07-25 14:34:46 +0530

    first commit

---
+++ car/src/main/java/marketing_tools/car/App.java
@@ -0,0 +1,13 @@
+package marketing_tools.car;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
